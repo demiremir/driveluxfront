781e8a2a8add50e46ff8606146ab861d1253c2ad
demiremir <d.eemirrr@gmail.com>
2023-06-07 12:33:28 +0300

    first
    first commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Login işlemlerini gerçekleştir
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div className="login-link">
+        Don't have an account? <a href="/signup">Sign Up</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/Signupform.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SignupForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Burada kayıt işlemlerini gerçekleştirebilirsiniz
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+      <div className="login-link">
+        Already have an account? <Link to="/">Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
