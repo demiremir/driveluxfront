5767f19388dbe1c6396e2e006764f6705d6f36af
demiremir <d.eemirrr@gmail.com>
2023-06-07 22:51:38 +0300

    edit

--- src/components/CarsPage.js
+++ src/components/CarsPage.js
@@ -2,33 +2,19 @@ import React from 'react';
 
 const CarsPage = () => {
   return (
-    <div className="page-container">
+    <div className="cars-page">
       <div className="sidebar">
-        <div className="sidebar-content">
-          <h2>Filters</h2>
-          <div className="filter-group">
-            <label htmlFor="model">Model:</label>
-            <input type="text" id="model" />
-          </div>
-          <div className="filter-group">
-            <label htmlFor="brand">Brand:</label>
-            <input type="text" id="brand" />
-          </div>
-          <div className="filter-group">
-            <label htmlFor="km">KM:</label>
-            <input type="text" id="km" />
-          </div>
-          <div className="filter-group">
-            <label htmlFor="gear-type">Gear Type:</label>
-            <input type="text" id="gear-type" />
-          </div>
+        <div className="filter-sidebar">
+          <h2 className="filter-label">Filters</h2>
+          <input type="text" className="filter-field" placeholder="Model" />
+          <input type="text" className="filter-field" placeholder="Brand" />
+          <input type="text" className="filter-field" placeholder="Km" />
+          <input type="text" className="filter-field" placeholder="Gear Type" />
+          <button className="filter-button">Filter</button>
         </div>
       </div>
-      <div className="content-container">
-        <div className="content">
-          <h2>Car List</h2>
-          {/* Araba listesi */}
-        </div>
+      <div className="car-list">
+        {/* Car List */}
       </div>
     </div>
   );
